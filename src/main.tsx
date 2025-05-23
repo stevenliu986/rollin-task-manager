b86ce1aae6ecd31ee9cb18acecc7c5c3921c5309
StevenLiu <stevenliu986@gmail.com>
2025-05-23 12:00:12 +1000

    feat: add route to AddTask page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import App from './App.tsx';
 import TaskList from './components/task/TaskList.tsx';
 import './index.css';
+import AddTask from './components/task/AddTask.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path='/taskList' element={<TaskList />} />
+      <Route path='/addTask' element={<AddTask />} />
     </Routes>
   </BrowserRouter>
 )
